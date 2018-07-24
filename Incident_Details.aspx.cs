3e842d13e71324c8d69165bfa22690634a6d9dc3
john1500463 <32880020+john1500463@users.noreply.github.com>
2018-07-24 12:43:41 +0200

    koloo

--- Incident_Details.aspx.cs
+++ Incident_Details.aspx.cs
@@ -4,16 +4,21 @@ using System.Data;
 using System.Data.SqlClient;
 using System.Diagnostics;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
 public partial class Incident_Details : System.Web.UI.Page
 {
+    
+        
     protected void Page_Load(object sender, EventArgs e)
     {
         String name;
         String Incident = Request.QueryString["ID"];
+       
         TableRow tRow;
         TableCell tCell;
         
